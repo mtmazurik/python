8ce155f97cfc28decf4c491cd80aee4f107e869b
Marty Mazurik <marty@martymazurik.com>
2020-04-18 11:36:01 -0700

    parse input string

--- CommandLineShell/CommandLineShell.py
+++ CommandLineShell/CommandLineShell.py
@@ -3,20 +3,35 @@ import subprocess
 import sys
 import logging
 
+def parseInput(inputString)
+    inputString = inputString.lower()   #lower case it all
+    inputs = split(inputString)         # break line by white space into inputs[] array
+
+    cmdstring = inputs[0]               # grab the first chars:   (t)empcalc command
+    cmd = cmdstring[0]                  # pluck of the first char
+
+    conversionType = cmdstring[1]       # grab the 2nd chars
+    temperatureValue = float(cmdstring[2])  # grab the 3rd chars and convert to float
+
+    cmd, converstionType, temperatureValue = parseInput()  # return 3 values from a function
+
 def main(): 
     print('type: e(xit) to leave')
     while True:  
+
         userInput = input('rsh> ')
-        if userInput == '' : continue
-        userInput = userInput.lower()
-        commandType = userInput[0]
-        if commandType == ('e'):
+
+        if userInput == '' : continue                    # handle no input case
+        cmd, convType, tempValue = parseInput(userInput) # separate the command line
+
+        if cmd == ('e'):    # (e)xit
             break
-        elif commandType == ('t'):
-            cmd_list = ['./Celcius_to_Fahrenheit_Calculator.py', sys.argv[1], sys.argv[2] ]
+        elif cmd == ('t'):  # (t)empcalc
+            cmd_list = ['./Celcius_to_Fahrenheit_Calculator.py', convType, tempValue ]
+
             p = subprocess.Popen(cmd_list, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
             stdout = p.stdout.read()
-            stderr = p.stderr.read()
+            
             print(stdout)
         else:
             print('Command not recognized. Re-enter a valid command.')
