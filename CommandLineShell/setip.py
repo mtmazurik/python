8324956c332e901db2728fa4b3750d7a9e36ad68
Marty Mazurik <marty@martymazurik.com>
2020-04-20 10:15:08 -0700

    fixup after installed pylint in vscode

--- CommandLineShell/setip.py
+++ CommandLineShell/setip.py
@@ -1,9 +1,10 @@
-# setip - network setting on ubuntu command
-# note: assuming 'eth0' is the network device for Virtual Machine ubuntu installations
-#
-# usage  rsh> setip 10.1.1.50 255.255.255.0 10.1.1.1       ( ip_address   subnet_mask   gateway)
+# setip.py - network setting on ubuntu command
+import sys
 import subprocess
 
+# setip        
+# usage  rsh> setip 10.1.1.50 255.255.255.0 10.1.1.1       ( ip_address   subnet_mask   gateway)
+# note: assuming 'eth0' is the network device for Virtual Machine ubuntu installations
 def main(): 
     if len(sys.argv) == 4:                           # we are being called by command line with arguments (note: will be lowercase convertType)
         ip_address = sys.argv[1]
@@ -12,13 +13,16 @@ def main():
     else:
         print('invalid input; example usage:  setip 10.1.1.50 255.255.255.0 10.1.1.1     ( ip_address subnet_mask  gateway)')
 
-    netInterface = 'eth0'        
+    netInterface = 'eth0'    
+    # linux command 1    
     ipargs = ['sudo', 'ifconfig', netInterface, ip_address, "netmask", mask ]
     raw1 = subprocess.check_output(ipargs)       # returns byte type
     outputMessage1 = raw1.decode('utf-8').rstrip() 
+    # linux command 2
     routeargs = ['sudo', 'route', 'add', 'default', "gw", gateway, netInterface ]
-    raw2 = subprocess.check_output()
+    raw2 = subprocess.check_output(routeargs)
     outputMessage2 = raw2.decode('utf-8').rstrip() 
+
     outputMessage = outputMessage1 + '/n' + outputMessage2
     print(outputMessage)
                           
@@ -27,4 +31,6 @@ if __name__ == '__main__':
         main()
     except:
         print('setip error encountered. Check command and retry.')
+        exc_type, exc_value, exc_tb = sys.exc_info()
+        print(f"Type: {exc_type}\nValue: {exc_value}\nTB: {exc_tb}")
         exit()
